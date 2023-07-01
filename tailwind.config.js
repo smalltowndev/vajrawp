388f91772d3fdf752c5e2b122bef58a87f7e8d03
Krishna Kant Chourasiya <lushkant@gmail.com>
2023-07-01 20:06:09 +0530

    Fix formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    important: '#wpbody-content',
-    content: [
-        "./src/dashboard/**/*.{html,js,ts,jsx,tsx}",
-        "./src/onboarding/**/*.{html,js,ts,jsx,tsx}"
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+	important: '#wpbody-content',
+	content: [
+		"./src/dashboard/**/*.{html,js,ts,jsx,tsx}",
+		"./src/onboarding/**/*.{html,js,ts,jsx,tsx}"
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
