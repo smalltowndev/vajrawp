c24309f4d14af05c0870ffbb9b298e502ba63d36
Krishna Kant Chourasiya <lushkant@gmail.com>
2023-07-26 23:22:14 +0530

    Update changelog and header

--- src/dashboard/components/parts/Header.jsx
+++ src/dashboard/components/parts/Header.jsx
@@ -4,17 +4,7 @@ import { Cog6ToothIcon } from "@heroicons/react/20/solid";
 import classNames from 'clsx';
 const pluginState = window.vajraPluginState;
 
-const navigation = [
-    { name: 'Dashboard', href: '/dashboard' },
-    { name: 'Getting Started', href: '/getting-started', icon: false },
-    { name: 'Changelog', href: '/changelog' },
-]
-
-const secondaryNav = [
-    { name: 'Settings', href: '/settings', icon: Cog6ToothIcon },
-]
-
-const Header = () => {
+const Header = ({ navigation, secondaryNav = null }) => {
     return (
         <Disclosure as="nav" className="bg-white shadow-sm">
             {({ open }) => (
@@ -31,47 +21,46 @@ const Header = () => {
                             </div>
                             <div>
                                 {/* @todo: Make version numbering dynamic and maybe show an update button when an update is available */}
-                                <div className="bg-gray-200 py-1 px-2 rounded-md font-bold text-gray-500 hover:text-gray-800">v0.1.0</div>
+                                <div className="bg-gray-200 py-1 px-2 rounded-md font-bold text-gray-500 hover:text-gray-800">v0.1.0-beta</div>
                             </div>
                         </div>
                         <div className="flex h-16 justify-between">
-                            <div className="flex">
-                                <div className="hidden sm:-my-px sm:flex sm:space-x-8">
-                                    {navigation.map((item) => (
-                                        <NavLink
-                                            key={item.name}
-                                            to={item.href}
-                                            className={({ isActive }) => classNames(
-                                                isActive
-                                                    ? 'border-gray-800 text-gray-900'
-                                                    : 'border-transparent text-gray-500 hover:border-gray-700 hover:text-gray-800',
-                                                'inline-flex items-center border-b-2 px-1 pt-1 text-base shadow-none'
-                                            )}
-                                        >
-                                            {item.name}
-                                        </NavLink>
-                                    ))}
+                            { navigation &&
+                                <div className="flex">
+                                    <div className="hidden sm:-my-px sm:flex sm:space-x-8">
+                                        {navigation.map((item) => (
+                                            <NavLink
+                                                key={item.name}
+                                                to={item.href}
+                                                className={({ isActive }) => classNames(
+                                                    isActive
+                                                        ? 'border-gray-800 text-gray-900'
+                                                        : 'border-transparent text-gray-500 hover:border-gray-700 hover:text-gray-800',
+                                                    'inline-flex items-center border-b-2 px-1 pt-1 text-base shadow-none'
+                                                )}
+                                            >
+                                                {item.name}
+                                            </NavLink>
+                                        ))}
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="flex">
-                                <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-                                    {secondaryNav.map((item) => (
-                                        <NavLink
-                                            key={item.name}
-                                            to={item.href}
-                                            className={({ isActive }) => classNames(
-                                                isActive
-                                                    ? 'border-gray-800 text-gray-900'
-                                                    : 'border-transparent text-gray-500 hover:border-gray-700 hover:text-gray-800',
-                                                'inline-flex items-center border-b-2 px-1.5 pt-1 text-base shadow-none'
-                                            )}
-                                        >
-                                            { item.icon && <item.icon className="h-4 w-4 mr-1.5 " /> }
-                                            {item.name}
-                                        </NavLink>
-                                    ))}
+                            }
+                            { secondaryNav &&
+                                <div className="flex">
+                                    <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-6">
+                                        {secondaryNav.map((item) => (
+                                            <NavLink
+                                                key={item.name}
+                                                to={item.href}
+                                                className="inline-flex items-center px-1.5 pt-1 text-base shadow-none text-gray-500 hover:text-gray-800"
+                                            >
+                                                { item.icon && <item.icon className="h-4 w-4 mr-1.5 " /> }
+                                                {item.name}
+                                            </NavLink>
+                                        ))}
+                                    </div>
                                 </div>
-                            </div>
+                            }
                         </div>
                     </div>
                 </>
