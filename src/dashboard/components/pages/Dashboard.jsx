2c2ae632879f2f8a56985efc9ea8aa721be5fa9d
Krishna Kant Chourasiya <lushkant@gmail.com>
2023-07-26 19:20:32 +0530

    Update dashboard styles

--- src/dashboard/components/pages/Dashboard.jsx
+++ src/dashboard/components/pages/Dashboard.jsx
@@ -45,7 +45,7 @@ const Dashboard = () => {
         <div className="overflow-hidden bg-white py-12 rounded">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none">
-                    <div className="lg:pr-8 lg:pt-4">
+                    <div className="lg:pr-8">
                         <h2 className="text-base font-semibold leading-7 text-gray-600">Build faster</h2>
                         <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Kickstart your WP Project!</p>
                         <p className="mt-6 text-lg leading-8 text-gray-600">
@@ -55,7 +55,7 @@ const Dashboard = () => {
                             {features.map((feature) => (
                                 <div key={feature.name} className="relative pl-9">
                                     <dt className="inline font-semibold text-gray-900">
-                                        <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-600" aria-hidden="true" />
+                                        <feature.icon className="absolute left-1 top-1 h-5 w-5 text-gray-700" aria-hidden="true" />
                                         {feature.name}
                                     </dt>{' '}
                                     <dd className="inline-block">{feature.description}</dd>
