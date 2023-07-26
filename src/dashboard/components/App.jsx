c8fda2e20f823ef1bab7d9c11a52d1b4e0fe6277
Krishna Kant Chourasiya <lushkant@gmail.com>
2023-07-26 23:22:05 +0530

    Update app dashboard and pages

--- src/dashboard/components/App.jsx
+++ src/dashboard/components/App.jsx
@@ -6,19 +6,18 @@ import { Route, Routes } from 'react-router-dom';
 /**
  * Internal dependencies.
  */
-import Layout from './Layout';
-import { Dashboard, Onboarding, Settings } from './pages';
+import { Dashboard, Onboarding, Changelog } from './pages';
+import { GeneralSettings, BlockSettings } from './pages/settings';
 
 const App = () => {
     return (
-        <Layout>
             <Routes>
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/getting-started" element={<Onboarding />} />
-                <Route path="/changelog" element={<h2>Changelog</h2>} />
-                <Route path="/settings" element={<Settings />} />
+                <Route path="/changelog" element={<Changelog />} />
+                <Route path="/settings" element={<GeneralSettings />} />
+                <Route path="/block-settings" element={<BlockSettings />} />
             </Routes>
-        </Layout>
     )
 }
 
--- src/dashboard/components/pages/index.jsx
+++ src/dashboard/components/pages/index.jsx
@@ -1,3 +1,4 @@
 export { default as Dashboard } from './Dashboard';
 export { default as Onboarding } from './Onboarding';
+export { default as Changelog } from './Changelog';
 export { default as Settings } from './Settings';
