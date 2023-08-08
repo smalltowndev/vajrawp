bbb660d5e0b20a20dd6b81e405acc88bf8195441
Krishna Kant Chourasiya <lushkant@gmail.com>
2023-08-09 02:34:47 +0530

    Improve Settings Fields and layout

---
+++ src/dashboard/components/templates/index.jsx
@@ -0,0 +1,5 @@
+export { default as SettingsCard } from './SettingsCard';
+
+// Settings fields.
+export { default as RadioSelectInput } from './RadioSelectInput';
+export { default as MultiSelectInput } from './MultiSelectInput';
