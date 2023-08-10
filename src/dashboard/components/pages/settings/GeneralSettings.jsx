d6eaf5537f1d700578eb813d729cb6ebf9e1432c
Krishna Kant Chourasiya <lushkant@gmail.com>
2023-08-10 16:33:55 +0530

    Add new input fields

--- src/dashboard/components/pages/settings/GeneralSettings.jsx
+++ src/dashboard/components/pages/settings/GeneralSettings.jsx
@@ -1,12 +1,25 @@
+/**
+ * External dependencies.
+ */
 import { useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies.
+ */
 import SettingsLayout from '../../layout/SettingsLayout';
-import { SettingsCard, RadioSelectInput, MultiSelectInput } from "../../templates";
+import { SettingsCard, RadioSelectInput, MultiSelectInput, SelectInput, TextInput, ToggleInput } from '../../templates';
 
 const GeneralSettings = () => {
 
     const [ options, setOptions ] = useState({
         "push-notif": "none",
-        "push-select": "same"
+        "push-select": [
+            "same",
+            "none"
+        ],
+        "select-notify": "default",
+        "site-meta-desc": "",
+        "site-meta-toggle": false
     });
 
     const updateOption = (value, id) => {
@@ -21,7 +34,32 @@ const GeneralSettings = () => {
     return (
         <SettingsLayout>
             <SettingsCard
-                title="Notifications"
+                title="Toggle fields"
+                description="We'll always let you know about important changes, but you pick what else you want to hear about."
+                onSave={onSave}
+            >
+
+                <TextInput
+                    id="site-meta-desc"
+                    label="Site Meta Description"
+                    description="These are delivered via SMS to your mobile phone."
+                    value={options["site-meta-desc"]}
+                    placeholder="Hello world"
+                    setOption={updateOption}
+                />
+                <ToggleInput
+                    id="site-meta-toggle"
+                    label="Site Meta Toggle"
+                    description="(Extra description)"
+                    value={options["site-meta-toggle"]}
+                    placeholder="Hello world"
+                    setOption={updateOption}
+                />
+
+            </SettingsCard>
+
+            <SettingsCard
+                title="Select Fields"
                 description="We'll always let you know about important changes, but you pick what else you want to hear about."
                 onSave={onSave}
             >
@@ -29,13 +67,13 @@ const GeneralSettings = () => {
                     id="push-select"
                     label="Push Notifications"
                     description="These are delivered via SMS to your mobile phone."
-                    defaultOption={options["push-select"]}
+                    values={options["push-select"]}
                     options={
-                        {
-                            everything: "Everything",
-                            same: "Same as the email",
-                            none: "No push notifications"
-                        }
+                        [
+                            { key: 'everything', label: "Everything", description: 'This does this and bla bla bla.' },
+                            { key: 'same', label: "Same as Email", description: 'This does this and bla bla bla.' },
+                            { key: 'none', label: "No Push Notifications", description: 'This does this and bla bla bla.' }
+                        ]
                     }
                     setOption={updateOption}
                 />
@@ -43,7 +81,21 @@ const GeneralSettings = () => {
                     id="push-notif"
                     label="Push Notifications"
                     description="These are delivered via SMS to your mobile phone."
-                    defaultOption={options["push-notif"]}
+                    value={options["push-notif"]}
+                    options={
+                        [
+                            { key: 'everything', label: "Everything", description: 'This does this and bla bla bla.' },
+                            { key: 'same', label: "Same as Email", description: 'This does this and bla bla bla.' },
+                            { key: 'none', label: "No Push Notifications", description: 'This does this and bla bla bla.' }
+                        ]
+                    }
+                    setOption={updateOption}
+                />
+                <SelectInput
+                    id="select-notify"
+                    label="Select Notifications"
+                    description="These are delivered via SMS to your mobile phone."
+                    value={options["select-notify"]}
                     options={
                         {
                             everything: "Everything",
--- src/dashboard/components/templates/MultiSelectInput.jsx
+++ src/dashboard/components/templates/MultiSelectInput.jsx
@@ -1,8 +1,10 @@
-const MultiSelectInput = ({ id, label, description, defaultOption, options, setOption }) => {
-    const filteredOptions = Object.entries( options );
-
-    const handleChange = e => {
-        setOption( e.target.value, id );
+const MultiSelectInput = ({ id, label, description, values, options, setOption, ...props }) => {
+    const handleChange = ( e, key ) => {
+        const updatedValues = e.target.checked ? values : values.filter((value) => value !== key);
+        setOption( [
+                ...updatedValues,
+            e.target.checked && key
+        ], id );
     };
 
     return (
@@ -10,25 +12,27 @@ const MultiSelectInput = ({ id, label, description, defaultOption, options, setO
         <fieldset>
             <legend className="text-sm font-semibold leading-6 text-gray-900">{label && label}</legend>
             <p className="mt-1 text-sm leading-6 text-gray-600">{description && description}</p>
-            { filteredOptions &&
+            { options &&
                 <div className="mt-6 space-y-6">
                     {
-                        filteredOptions.map( ( [key, value], index ) =>
-                            <div key={index} className="relative flex gap-x-3">
+                        options.map( ( option ) =>
+                            <div key={option.key} className="relative flex gap-x-3">
                                 <div className="flex h-6 items-center">
                                     <input
-                                        id={id+`[${key}]`}
+                                        id={id+`[${option.key}]`}
                                         name={id}
                                         type="checkbox"
-                                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                        defaultChecked={defaultOption === key}
+                                        className="h-4 w-4 shadow-none rounded border-gray-300 text-gray-600 focus:ring-gray-600"
+                                        onChange={(e) => handleChange(e, option.key)}
+                                        defaultChecked={values.includes(option.key)}
+                                        {...props}
                                     />
                                 </div>
                                 <div className="text-sm leading-6">
-                                    <label htmlFor={id+`[${key}]`} className="font-medium text-gray-900">
-                                        {value}
+                                    <label htmlFor={id+`[${option.key}]`} className="font-medium text-gray-900">
+                                        {option?.label}
                                     </label>
-                                    {/*<p className="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>*/}
+                                    <p className="text-gray-500">{option?.description}</p>
                                 </div>
                             </div>
 
